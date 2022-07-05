3da02d57378c1810f9e83bc465960124c119e29e
Samuel <20127610@student.hcmus.edu.vn>
2022-07-05 19:57:37 +0700

    connect to db

--- main.go
+++ main.go
@@ -4,7 +4,33 @@ import(
 	"fmt"
 	// "net/http"
 	// "github.com/labstack/echo/v4"
+	"github.com/beego/beego/v2/client/orm"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+func init() {
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+
+	err := orm.RegisterDataBase("default", "mysql", "root:123456@/echotest?charset=utf8")
+	if err != nil{
+		fmt.Println("Failed to connect database", err)
+	}
+
+	// Database alias
+	name := "default"
+
+	// Drop table and re-create
+	force := true
+
+	// Print log
+	verbose := true
+
+	// Error
+	err = orm.RunSyncdb(name, force, verbose)
+	if err != nil{
+		fmt.Println("Failed to run sync", err)
+	}
+}
 
 func main() {
 	fmt.Println("Hello, world.")
